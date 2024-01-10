e68f2a1954157bf4fdfd498264867bf0faa08ddb
boraaktas <bora.aktas@smartopt.com.tr>
2024-01-10 09:51:08 +0300

    metaheuristic 3

--- MHs/Solution_Rep.py
+++ MHs/Solution_Rep.py
@@ -3,6 +3,9 @@ from functools import lru_cache
 
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels
+import sklearn
 
 from InputHandler import InputHandler
 from Solution_Object import Solution_Object
@@ -20,18 +23,17 @@ class Solution_Rep:
         self.init_solutions = input_h.solutions
         self.no_timestamps = int(input_h.no_timestamps)
 
-        self.N_List = [self.N1, self.N2, self.N3, self.N4, self.N5, self.N6]
+        self.N_List = [self.N1, self.N2, self.N3, self.N4, self.N5, self.N6, self.N7]
 
         # !!!!!!
         # make sure that name of the keys are same with the name of the attributes in Solution_Object
         # !!!!!!
         dict_keys = ['I_KOMP1_HIZ', 'II_KOMP1_HIZ', 'III_KOMP1_HIZ', 'IV_KOMP1_HIZ',
-                     'KS10_UDP_TUKETIM',
                      'CH1_CIKIS_SIC', 'CH1_GIRIS_SIC',
                      'CH2_CIKIS_SIC', 'CH2_GIRIS_SIC',
                      'CH3_CIKIS_SIC', 'CH3_GIRIS_SIC',
                      'SENSOR_I_TEMP', 'SENSOR_II_TEMP',
-                     'OUTLET_TEMP', 'OUTLET_HUMIDITY',
+                     'OUTLET_TEMP', 'OUTLET_HUMIDITY', 'KS10_UDP_TUKETIM',
                      'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat',
                      'hour_0', 'hour_1', 'hour_2', 'hour_3', 'hour_4',
                      'hour_5', 'hour_6', 'hour_7', 'hour_8', 'hour_9',
@@ -42,14 +44,14 @@ class Solution_Rep:
                      'month_8', 'month_9', 'month_10',
                      'PUE_lag_one_day']
 
-        self.PUE_DICT = dict.fromkeys(dict_keys, 0)
+        self.PUE_DICT_OLS = dict.fromkeys(dict_keys, 0)
 
         # remove SENSOR_I_TEMP and SENSOR_II_TEMP from dict_keys
-        sensor_dict_keys = dict_keys.copy()
+        sensor_dict_keys = dict_keys.copy()[:-38]
         sensor_dict_keys.remove('SENSOR_I_TEMP')
         sensor_dict_keys.remove('SENSOR_II_TEMP')
-        self.SENSOR_I_DICT = dict.fromkeys(sensor_dict_keys, 0)
-        self.SENSOR_II_DICT = dict.fromkeys(sensor_dict_keys, 0)
+        self.SENSOR_I_DICT_OLS = dict.fromkeys(sensor_dict_keys, 0)
+        self.SENSOR_II_DICT_OLS = dict.fromkeys(sensor_dict_keys, 0)
 
     @staticmethod
     def dict_to_predict(s_timestamp: Solution_Object,
@@ -75,6 +77,8 @@ class Solution_Rep:
 
         for i in range(len(dummies_name)):
             copy_dict[dummies_name[i]] = s_timestamp_dict['DUMMIES'][i]
+
+        # copy_dict = list(copy_dict.values())
 
         return copy_dict
 
@@ -118,10 +122,16 @@ class Solution_Rep:
                     init_speed = s_timestamp.__dict__[f'init_{roman_numbers[j]}_KOMP1_HIZ']
                     speed = s_timestamp.__dict__[f'{roman_numbers[j]}_KOMP1_HIZ']
 
-                    if init_speed != 0:
+                    if speed <= 25.1:
+                        objective_timestamp += 10 ** 8
+
+                    if speed >= 74.9:
+                        objective_timestamp += 10 ** 8
+
+                    '''if init_speed != 0:
                         speed_change_percentage = abs(init_speed - speed) / init_speed
                         if speed_change_percentage > 0.15:
-                            objective_timestamp += (10 ** 5) * speed_change_percentage
+                            objective_timestamp += (10 ** 5) * speed_change_percentage'''
 
             # else every komp1_hiz it should be im range of s_timestamp_one_before.init_..._KOMP1_HIZ * 0.85 and
             # s_timestamp_one_before.init_..._KOMP1_HIZ * 1.15
@@ -134,10 +144,10 @@ class Solution_Rep:
 
                     if speed_one_before != 0:
                         speed_change_percentage = abs(speed_one_before - speed) / speed_one_before
-                        if speed_change_percentage > 0.15:
-                            objective_timestamp += (10 ** 5) * speed_change_percentage
+                        if speed_change_percentage > 0.25:
+                            objective_timestamp += (10 ** 1) * speed_change_percentage
 
-            total_objective += objective_timestamp * (self.no_timestamps - i)
+            total_objective += objective_timestamp * (self.no_timestamps - i) / 10
 
         objective = total_objective / sum(range(self.no_timestamps + 1))
 
@@ -152,19 +162,19 @@ class Solution_Rep:
         # if sensor_i_temp or sensor_ii_temp is +-4 than set temperatures, give a high penalty
         sic_set_penalty = 0
         if sensor_i_temp - s_timestamp.SIC_I_SET > 4:
-            sic_set_penalty *= 10 ** 1
+            sic_set_penalty *= 10 ** 8
         if s_timestamp.SIC_I_SET - sensor_i_temp > 4:
-            sic_set_penalty += 10 ** 2
-        if sensor_ii_temp - s_timestamp.SIC_II_SET > 4:
             sic_set_penalty += 10 ** 1
+        if sensor_ii_temp - s_timestamp.SIC_II_SET > 4:
+            sic_set_penalty += 10 ** 8
         if s_timestamp.SIC_II_SET - sensor_ii_temp > 4:
-            sic_set_penalty += 10 ** 2
+            sic_set_penalty += 10 ** 1
 
         # if PUE is different from 1, give a penalty as much as difference
         # the more PUE is different than 1, the more penalty with exponential
         pue_penalty = 0
         diff_PUE = PUE - 1
-        pue_penalty += 10 ** 0 * (diff_PUE / 1)
+        pue_penalty += 10 ** 4 * (diff_PUE / 0.1)
 
         objective_timestamp = pue_penalty + sic_set_penalty
 
@@ -176,18 +186,26 @@ class Solution_Rep:
         sensor_i_temp = self.predict_SENSOR_I_TEMP(s_timestamp)
         sensor_ii_temp = self.predict_SENSOR_II_TEMP(s_timestamp)
 
-        PUE_SAMPLE_DICT = self.dict_to_predict(s_timestamp, self.PUE_DICT)
+        PUE_SAMPLE_DICT = self.dict_to_predict(s_timestamp, self.PUE_DICT_OLS)
         PUE_SAMPLE_DICT['SENSOR_I_TEMP'] = sensor_i_temp
         PUE_SAMPLE_DICT['SENSOR_II_TEMP'] = sensor_ii_temp
 
-        PUE = np.exp(self.PUE_MODEL.predict(PUE_SAMPLE_DICT)[0]) + 1
+        PUE = 2
+        if type(self.PUE_MODEL) == statsmodels.regression.linear_model.RegressionResultsWrapper:
+            PUE = np.exp(self.PUE_MODEL.predict(PUE_SAMPLE_DICT)[0]) + 1
+        elif type(self.PUE_MODEL) == sklearn.ensemble._forest.RandomForestRegressor:
+            PUE = np.exp(self.PUE_MODEL.predict([list(PUE_SAMPLE_DICT.values())])[0]) + 1
+        elif type(self.PUE_MODEL) == sklearn.ensemble._gb.GradientBoostingRegressor:
+            PUE = np.exp(self.PUE_MODEL.predict([list(PUE_SAMPLE_DICT.values())])[0]) + 1
+        else:
+            raise Exception('PUE_MODEL is not a valid model!')
 
         return PUE, sensor_i_temp, sensor_ii_temp
 
     def predict_SENSOR_I_TEMP(self: 'Solution_Rep',
                               s_timestamp: Solution_Object) -> float:
 
-        SENSOR_I_SAMPLE_DICT = self.dict_to_predict(s_timestamp, self.SENSOR_I_DICT)
+        SENSOR_I_SAMPLE_DICT = self.dict_to_predict(s_timestamp, self.SENSOR_I_DICT_OLS)
 
         SENSOR_I_TEMP = np.exp(self.SENSOR_I_MODEL.predict(SENSOR_I_SAMPLE_DICT)[0])
 
@@ -196,7 +214,7 @@ class Solution_Rep:
     def predict_SENSOR_II_TEMP(self: 'Solution_Rep',
                                s_timestamp: Solution_Object) -> float:
 
-        SENSOR_II_SAMPLE_DICT = self.dict_to_predict(s_timestamp, self.SENSOR_II_DICT)
+        SENSOR_II_SAMPLE_DICT = self.dict_to_predict(s_timestamp, self.SENSOR_II_DICT_OLS)
 
         SENSOR_II_TEMP = np.exp(self.SENSOR_II_MODEL.predict(SENSOR_II_SAMPLE_DICT)[0])
 
@@ -329,6 +347,21 @@ class Solution_Rep:
 
         return speed_list, (random_timestamp_index, random_percentage)
 
+    def N7(self: 'Solution_Rep',
+           speed_list: list) -> (list, tuple):
+        """
+        Sets all timestamps to the their initial speeds.
+        :param speed_list:
+        :return:
+        """
+        for i in range(self.no_timestamps):
+            s_timestamp = self.init_solutions[i]
+            speed_list[i] = (s_timestamp.init_I_KOMP1_HIZ,
+                             s_timestamp.init_II_KOMP1_HIZ,
+                             s_timestamp.init_III_KOMP1_HIZ,
+                             s_timestamp.init_IV_KOMP1_HIZ)
+
+        return speed_list, (-1, -1)
 
     def excel_write(self: 'Solution_Rep',
                     speed_list: list,
@@ -375,3 +408,130 @@ class Solution_Rep:
             print('SENSOR_II_TEMP: ' + str(format(s_timestamp.SENSOR_II_TEMP, '.4f')) +
                   ' ----- ' + 'SIC_II_SET: ' + str(format(s_timestamp.SIC_II_SET, '.4f')))
             print()
+
+        # print PUEs
+        PUEs = []
+        for i in range(self.no_timestamps):
+            PUEs.append(solutions[i].PUE)
+
+        print('PUEs: ' + str(PUEs))
+
+        # create a dataframe to write excel file
+        OUTPUT_DATA = pd.read_csv(output_path)
+        OUTPUT_DATA['Timestamp'] = pd.to_datetime(OUTPUT_DATA['Timestamp'])
+
+        # OUTPUT_DATA columns:
+        #       ['Timestamp', 'PUE', 'I_KOMP1_HIZ', 'II_KOMP1_HIZ', 'III_KOMP1_HIZ',
+        #        'IV_KOMP1_HIZ', 'CH1_CIKIS_SIC', 'CH1_GIRIS_SIC', 'CH2_CIKIS_SIC',
+        #        'CH2_GIRIS_SIC', 'CH3_CIKIS_SIC', 'CH3_GIRIS_SIC', 'SENSOR_I_TEMP',
+        #        'SENSOR_II_TEMP', 'OUTLET_TEMP', 'OUTLET_HUMIDITY', 'KS10_UDP_TUKETIM',
+        #        'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'hour_0', 'hour_1', 'hour_2',
+        #        'hour_3', 'hour_4', 'hour_5', 'hour_6', 'hour_7', 'hour_8', 'hour_9',
+        #        'hour_10', 'hour_11', 'hour_12', 'hour_13', 'hour_14', 'hour_15',
+        #        'hour_16', 'hour_17', 'hour_18', 'hour_19', 'hour_20', 'hour_21',
+        #        'hour_22', 'hour_23',
+        #        'month_4', 'month_5', 'month_6', 'month_7',
+        #        'month_8', 'month_9', 'month_10', 'month_11',
+        #        'KS10_REAL_CH1_CIKIS_SIC', 'KS10_REAL_CH1_GIRIS_SIC',
+        #        'KS10_REAL_CH2_CIKIS_SIC', 'KS10_REAL_CH2_GIRIS_SIC',
+        #        'KS10_REAL_CH3_CIKIS_SIC', 'KS10_REAL_CH3_GIRIS_SIC',
+        #        'KS10_REAL_OUTLET_TEMP', 'KS10_REAL_OUTLET_HUMIDITY',
+        #        'KS10_REAL_KS10_UDP_TUKETIM', 'solved_for_first_timestamp']
+
+        # insert new timestamps to OUTPUT_DATA
+        for i in range(self.no_timestamps):
+            s_timestamp = solutions[i]
+
+            if s_timestamp.timestamp not in OUTPUT_DATA['Timestamp'].values:
+                OUTPUT_DATA.loc[len(OUTPUT_DATA)] = [s_timestamp.timestamp] + [None] * len(OUTPUT_DATA.columns[1:])
+
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'PUE'] = s_timestamp.PUE
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'I_KOMP1_HIZ'] = s_timestamp.I_KOMP1_HIZ
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'II_KOMP1_HIZ'] = s_timestamp.II_KOMP1_HIZ
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'III_KOMP1_HIZ'] = s_timestamp.III_KOMP1_HIZ
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'IV_KOMP1_HIZ'] = s_timestamp.IV_KOMP1_HIZ
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'CH1_CIKIS_SIC'] = s_timestamp.CH1_CIKIS_SIC
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'CH1_GIRIS_SIC'] = s_timestamp.CH1_GIRIS_SIC
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'CH2_CIKIS_SIC'] = s_timestamp.CH2_CIKIS_SIC
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'CH2_GIRIS_SIC'] = s_timestamp.CH2_GIRIS_SIC
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'CH3_CIKIS_SIC'] = s_timestamp.CH3_CIKIS_SIC
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'CH3_GIRIS_SIC'] = s_timestamp.CH3_GIRIS_SIC
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'SENSOR_I_TEMP'] = s_timestamp.SENSOR_I_TEMP
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'SENSOR_II_TEMP'] = s_timestamp.SENSOR_II_TEMP
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'OUTLET_TEMP'] = s_timestamp.OUTLET_TEMP
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'OUTLET_HUMIDITY'] = s_timestamp.OUTLET_HUMIDITY
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_UDP_TUKETIM'] = s_timestamp.KS10_UDP_TUKETIM
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'Mon'] = s_timestamp.DUMMIES[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'Tue'] = s_timestamp.DUMMIES[1]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'Wed'] = s_timestamp.DUMMIES[2]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'Thu'] = s_timestamp.DUMMIES[3]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'Fri'] = s_timestamp.DUMMIES[4]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'Sat'] = s_timestamp.DUMMIES[5]
+            for h in range(24):
+                OUTPUT_DATA.loc[
+                    OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, f'hour_{h}'] = s_timestamp.DUMMIES[6 + h]
+            for m in range(4, 12):
+                OUTPUT_DATA.loc[
+                    OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, f'month_{m}'] = s_timestamp.DUMMIES[26 + m]
+
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_PUE'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['PUE'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_CH1_CIKIS_SIC'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['CH1_CIKIS_SIC'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_CH1_GIRIS_SIC'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['CH1_GIRIS_SIC'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_CH2_CIKIS_SIC'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['CH2_CIKIS_SIC'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_CH2_GIRIS_SIC'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['CH2_GIRIS_SIC'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_CH3_CIKIS_SIC'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['CH3_CIKIS_SIC'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_CH3_GIRIS_SIC'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['CH3_GIRIS_SIC'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_OUTLET_TEMP'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['OUTLET_TEMP'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_OUTLET_HUMIDITY'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['OUTLET_HUMIDITY'].values[0]
+            OUTPUT_DATA.loc[
+                OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'KS10_REAL_KS10_UDP_TUKETIM'] = \
+                KS10_REAL_DATA[KS10_REAL_DATA['Timestamp'] == s_timestamp.timestamp]['KS10_UDP_TUKETIM'].values[0]
+
+            if i == 0:
+                OUTPUT_DATA.loc[
+                    OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'solved_for_first_timestamp'] = 1
+            else:
+                OUTPUT_DATA.loc[
+                    OUTPUT_DATA['Timestamp'] == s_timestamp.timestamp, 'solved_for_first_timestamp'] = 0
+
+        OUTPUT_DATA.to_csv(output_path, index=False)
--- MHs/main.py
+++ MHs/main.py
@@ -7,7 +7,7 @@ from Solution_Rep import Solution_Rep
 import os
 
 REAL_DATA_PATH = '../data/KS VERI/KS10_FINAL_DATA.csv'
-OUTPUT_DATA_PATH = '../data/KS VERI/KS10_MH_OUTPUT_DATA.csv'
+OUTPUT_DATA_PATH = '../data/KS VERI/KS10_MH_OUTPUT_DATA_SA.csv'
 
 PICKLES_PATH = 'pickles/'
 
@@ -25,7 +25,7 @@ CH3_GIRIS_SIC_MODEL_PATH = PICKLES_PATH + 'ARIMA_CH3_GIRIS_SIC_MODEL.pkl'
 KS10_UDP_TUKETIM_MODEL_PATH = PICKLES_PATH + 'ARIMA_KS10_UDP_TUKETIM_MODEL.pkl'
 
 START_TIMESTAMP = '2023-10-18 08:00:00'
-NO_TIMESTAMPS = 3
+NO_TIMESTAMPS = 10
 
 # create KS10_REAL_DATA to start
 # The first timestamp of KS10_REAL_DATA is 2023-04-01 00:10:00
@@ -34,7 +34,51 @@ KS10_REAL_DATA = pd.read_csv(REAL_DATA_PATH)
 KS10_REAL_DATA['Timestamp'] = pd.to_datetime(KS10_REAL_DATA['Timestamp'])
 
 # create an empty KS10_OUTPUT_DATA
-KS10_OUTPUT_DATA = pd.DataFrame(columns=KS10_REAL_DATA.columns)
+# KS10_REAL_DATA columns:
+#       ['Timestamp', 'PUE', 'I_KOMP1_HIZ', 'I_KOMP1_SAAT', 'I_KOMP2_HIZ',
+#        'I_KOMP2_SAAT', 'I_NEM_SET', 'I_SIC_SET', 'II_KOMP1_HIZ',
+#        'II_KOMP1_SAAT', 'II_KOMP2_HIZ', 'II_KOMP2_SAAT', 'II_NEM_SET',
+#        'II_SIC_SET', 'III_KOMP1_HIZ', 'III_KOMP1_SAAT', 'III_KOMP2_HIZ',
+#        'III_KOMP2_SAAT', 'III_NEM_SET', 'III_SIC_SET', 'IV_KOMP1_HIZ',
+#        'IV_KOMP1_SAAT', 'IV_KOMP2_HIZ', 'IV_KOMP2_SAAT', 'IV_NEM_SET',
+#        'IV_SIC_SET', 'CH1_CIKIS_SIC', 'CH1_GIRIS_SIC', 'CH2_CIKIS_SIC',
+#        'CH2_GIRIS_SIC', 'CH3_CIKIS_SIC', 'CH3_GIRIS_SIC', 'SENSOR_I_TEMP',
+#        'SENSOR_II_TEMP', 'OUTLET_TEMP', 'OUTLET_HUMIDITY', 'KS10_UDP_TUKETIM',
+#        'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'hour_0', 'hour_1', 'hour_2',
+#        'hour_3', 'hour_4', 'hour_5', 'hour_6', 'hour_7', 'hour_8', 'hour_9',
+#        'hour_10', 'hour_11', 'hour_12', 'hour_13', 'hour_14', 'hour_15',
+#        'hour_16', 'hour_17', 'hour_18', 'hour_19', 'hour_20', 'hour_21',
+#        'hour_22', 'hour_23', 'month_1', 'month_2', 'month_3', 'month_4',
+#        'month_5', 'month_6', 'month_7', 'month_8', 'month_9', 'month_10',
+#        'month_11']
+# drop I_KOMP1_SAAT, I_KOMP2_SAAT, II_KOMP1_SAAT, II_KOMP2_SAAT,
+# III_KOMP1_SAAT, III_KOMP2_SAAT, IV_KOMP1_SAAT, IV_KOMP2_SAAT
+# drop I_KOMP2_HIZ, II_KOMP2_HIZ, III_KOMP2_HIZ, IV_KOMP2_HIZ
+# drop I_NEM_SET, II_NEM_SET, III_NEM_SET, IV_NEM_SET
+# drop I_SIC_SET, II_SIC_SET, III_SIC_SET, IV_SIC_SET
+KS10_REAL_DATA = KS10_REAL_DATA.drop(columns=['I_KOMP1_SAAT', 'I_KOMP2_SAAT',
+                                              'II_KOMP1_SAAT', 'II_KOMP2_SAAT',
+                                              'III_KOMP1_SAAT', 'III_KOMP2_SAAT',
+                                              'IV_KOMP1_SAAT', 'IV_KOMP2_SAAT',
+                                              'I_KOMP2_HIZ', 'II_KOMP2_HIZ',
+                                              'III_KOMP2_HIZ', 'IV_KOMP2_HIZ',
+                                              'I_NEM_SET', 'II_NEM_SET',
+                                              'III_NEM_SET', 'IV_NEM_SET',
+                                              'I_SIC_SET', 'II_SIC_SET',
+                                              'III_SIC_SET', 'IV_SIC_SET',
+                                              'month_1', 'month_2', 'month_3'])
+
+KS10_OUTPUT_DATA = pd.DataFrame(columns=list(KS10_REAL_DATA.columns) + ['KS10_REAL_PUE',
+                                                                        'KS10_REAL_CH1_CIKIS_SIC',
+                                                                        'KS10_REAL_CH1_GIRIS_SIC',
+                                                                        'KS10_REAL_CH2_CIKIS_SIC',
+                                                                        'KS10_REAL_CH2_GIRIS_SIC',
+                                                                        'KS10_REAL_CH3_CIKIS_SIC',
+                                                                        'KS10_REAL_CH3_GIRIS_SIC',
+                                                                        'KS10_REAL_OUTLET_TEMP',
+                                                                        'KS10_REAL_OUTLET_HUMIDITY',
+                                                                        'KS10_REAL_KS10_UDP_TUKETIM',
+                                                                        'solved_for_first_timestamp'])
 
 KS10_OUTPUT_DATA.to_csv(OUTPUT_DATA_PATH, index=False)
 
@@ -59,20 +103,24 @@ InputHandler.set_CH_GIRIS_SIC_MODEL([CH1_GIRIS_SIC_MODEL_PATH,
 InputHandler.set_KS10_UDP_TUKETIM_MODEL(KS10_UDP_TUKETIM_MODEL_PATH)
 
 # create solution object
-input_h = InputHandler(start_timestamp=START_TIMESTAMP,
-                       no_timestamps=NO_TIMESTAMPS,
-                       output_path=OUTPUT_DATA_PATH)
 
-sol = Solution_Rep(input_h)
+for i in range(100):
+    start_timestamp = str(pd.to_datetime(START_TIMESTAMP) + pd.Timedelta(minutes=5 * i))
 
-'''TS_result = TS(sol.init, sol.N_List, sol.objective,
-               tabu_size=10, num_neighbors=30,
-               time_limit=60, ITER=1000000, print_results=False, print_iteration=True)'''
+    input_h = InputHandler(start_timestamp=start_timestamp,
+                           no_timestamps=NO_TIMESTAMPS,
+                           output_path=OUTPUT_DATA_PATH)
 
-SA_result = SA(sol.init, sol.N_List, sol.objective,
-               time_limit=30, ITER=1000000, print_results=False, print_iteration=True)
+    sol = Solution_Rep(input_h)
 
-meta_sol = SA_result
-# objective_meta = sol.objective(meta_sol)
+    '''TS_result = TS(sol.init, sol.N_List, sol.objective,
+                   tabu_size=10, num_neighbors=30,
+                   time_limit=60, ITER=1000000, print_results=False, print_iteration=True)'''
 
-sol.excel_write(meta_sol, OUTPUT_DATA_PATH, KS10_REAL_DATA)
+    SA_result = SA(sol.init, sol.N_List, sol.objective,
+                   time_limit=20, ITER=1000000, print_results=False, print_iteration=False)
+
+    meta_sol = SA_result
+    # objective_meta = sol.objective(meta_sol)
+
+    sol.excel_write(meta_sol, OUTPUT_DATA_PATH, KS10_REAL_DATA)
